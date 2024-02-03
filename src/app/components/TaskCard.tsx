8876a0379129921d1732cabbad970d4f5dcec46e
Ryan Furrer <ryandotfurrer@gmail.com>
2024-02-03 07:34:09 -0500

    Implement state on `TaskCard.tsx` for interactivity

--- src/app/components/TaskCard.tsx
+++ src/app/components/TaskCard.tsx
@@ -9,18 +9,16 @@ type TaskCardProps = {
 
 export default function TaskCard({ taskName, taskDescription }: TaskCardProps) {
   const [isChecked, setIsChecked] = useState(false);
-  const taskCard = document.querySelector(`#${taskName}`);
 
   const checkHandler = () => {
     setIsChecked(!isChecked);
-    if (!isChecked) {
-      taskCard.classList.toggle("bg-red-800");
-    }
   };
+
+  const dynamicClassName = isChecked ? "opacity-50" : "";
 
   return (
     <div
-      className="flow mx-auto max-w-prose rounded-md border p-8"
+      className={`flow mx-auto max-w-prose rounded-md border p-8 ${dynamicClassName}`}
       id="taskCard"
     >
       <h3>{taskName}</h3>
