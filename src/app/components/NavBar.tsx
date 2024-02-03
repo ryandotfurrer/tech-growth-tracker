d1d90c804beeba36fe0f11c9d260f528b6842715
Ryan Furrer <ryandotfurrer@gmail.com>
2024-02-03 08:54:58 -0500

    Add Tech Growth Tracker logo

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import logo from "/public/logo.webp";
+import logo from "/public/tgt-logo.webp";
 import Link from "next/link";
 
 export default function NavBar() {
